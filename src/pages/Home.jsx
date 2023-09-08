f1649ac9c7bf4e1af8fed88cbfa7089b8aed822b
sidharthtripathi <sidharthtripathi807@gmail.com>
2023-09-08 21:55:50 +0530

    redirect to login page when not loggen in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CreatePost from '../components/CreatePost'
 import HomeFeed from '../components/HomeFeed'
 function Home() {
+  if(!localStorage.getItem('auth-token')) window.location = '/login'
   return (
     <div className='h-screen w-screen flex justify-center'>
       <main className='w-1/2  overflow-y-scroll'>
