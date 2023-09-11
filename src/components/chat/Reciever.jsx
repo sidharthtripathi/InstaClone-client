62e973ab1371c9addfefb06ba058027ac7ce62f7
sidharthtripathi <sidharthtripathi807@gmail.com>
2023-09-11 06:40:21 +0530

    removed scrollbar visibility, all messages are re rendering on single msg update

--- src/components/chat/Reciever.jsx
+++ src/components/chat/Reciever.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Reciever({value}) {
   return (
-    <div className='w-fit ml-3 p-2 rounded-md my-1 bg-green-500 text-white'>
+    <div className='w-fit ml-3 p-2 rounded-md my-1 bg-green-500 text-white '>
       {value}
 
     </div>
