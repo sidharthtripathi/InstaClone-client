8ba2399f7dbba346f1046c89febd207662740dc7
sidharthtripathi <sidharthtripathi807@gmail.com>
2023-09-13 12:25:47 +0530

    fixed unfollow crash

--- src/components/FollowStatus.jsx
+++ src/components/FollowStatus.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Pending from './Pending'
 import FollowBtn from './FollowBtn'
+import Unfollow from './Unfollow'
 function FollowStatus({status}) {
     if(status == 'none')
         return <FollowBtn/>
